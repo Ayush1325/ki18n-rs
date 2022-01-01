3ed28937393384220d45790c3b696d5eb7688055
Ayush Singh <ayushdevel1325@gmail.com>
2022-01-01 20:35:14 +0530

    Added Methods for klocalizedtranslator.

--- src/klocalizedtranslator.rs
+++ src/klocalizedtranslator.rs
@@ -1,4 +1,5 @@
 use cpp::{cpp, cpp_class};
+use qttypes::QString;
 
 #[cfg(feature = "qmetaobject")]
 use qmetaobject::{QObject, QObjectPinned};
@@ -26,5 +27,26 @@ impl KLocalizedTranslator {
         cpp!(unsafe [obj_ptr as "QObject *"] -> KLocalizedTranslator as "KLocalizedTranslatorHolder" {
             return KLocalizedTranslatorHolder(obj_ptr);
         })
+    }
+
+    /// Adds a context for which this Translator should be active.
+    pub fn add_context_to_monitor(&mut self, context: QString) {
+        cpp!(unsafe [self as "KLocalizedTranslatorHolder *", context as "QString"] {
+            self->translator->addContextToMonitor(context);
+        });
+    }
+
+    /// Stop translating for the given context.
+    pub fn remove_context_to_moitor(&mut self, context: QString) {
+        cpp!(unsafe [self as "KLocalizedTranslatorHolder *", context as "QString"] {
+            self->translator->removeContextToMonitor(context);
+        });
+    }
+
+    /// Sets the translationDomain to be used.
+    pub fn set_translation_domain(&mut self, translation_domain: QString) {
+        cpp!(unsafe [self as "KLocalizedTranslatorHolder *", translation_domain as "QString"] {
+            self->translator->setTranslationDomain(translation_domain);
+        });
     }
 }
